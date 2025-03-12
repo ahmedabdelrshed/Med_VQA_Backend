7751e1f7231ec979828fa9c7ea6b60045d39b95e
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-03-12 07:04:22 +0200

    update send reset password email

--- utils/sendResetPasswordEmail.js
+++ utils/sendResetPasswordEmail.js
@@ -1,75 +1,75 @@
-// require("dotenv").config();
-// const nodemailer = require("nodemailer");
-// const appError = require("./appError");
-// const { ERROR } = require("./httpStatus");
-// const transporter = nodemailer.createTransport({
-//   service: "Gmail",
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.EMAIL_PASSWORD,
-//   },
-//   pool: true,
-//   maxConnections: 1,
-//   maxMessages: 5,
-// });
-// const sendResetPasswordEmail = async (email, token) => {
-//   const url = `${process.env.FRONTEND_URL}/change_password/${token}`;
-//   const message = {
-//     from: process.env.EMAIL,
-//     to: email,
-//     subject: "Reset Password Request",
-//     html: `
-//       <h1>Reset Password Request</h1>
-//       <p>You requested a password reset for your Med VQA account.</p>
-//       <p>Please click the following link to reset your password:</p>
-//       <a href=${url}>Reset Password</a>
-//       <p>If you did not request a password reset, please ignore this email.</p>
-//       `,
-//   };
-//   transporter.sendMail(message, (err, info) => {
-//     if (err) {
-//       throw appError.createError(
-//         "Error Occur When Send Reset Password Email Please try again",
-//         400,
-//         ERROR
-//       );
-//     }
-//   });
-// };
-
-// module.exports = sendResetPasswordEmail;
 require("dotenv").config();
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 const appError = require("./appError");
 const { ERROR } = require("./httpStatus");
-
-
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+  pool: true,
+  maxConnections: 1,
+  maxMessages: 5,
+});
 const sendResetPasswordEmail = async (email, token) => {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  try {
-    const url = `${process.env.FRONTEND_URL}/change_password/${token}`;
-    const message = {
-      from: process.env.EMAIL,
-      to: email,
-      subject: "Reset Password Request",
-      html: `
-        <h1>Reset Password Request</h1>
-        <p>You requested a password reset for your Med VQA account.</p>
-        <p>Please click the following link to reset your password:</p>
-        <a href=${url}>Reset Password</a>
-        <p>If you did not request a password reset, please ignore this email.</p>
+  const url = `${process.env.FRONTEND_URL}/change_password/${token}`;
+  const message = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: "Reset Password Request",
+    html: `
+      <h1>Reset Password Request</h1>
+      <p>You requested a password reset for your Med VQA account.</p>
+      <p>Please click the following link to reset your password:</p>
+      <a href=${url}>Reset Password</a>
+      <p>If you did not request a password reset, please ignore this email.</p>
       `,
-    };
-
-    await sgMail.send(message);
-  } catch (error) {
-    throw appError.createError(
-      "Error Occurred While Sending Reset Password Email. Please Try Again.",
-      400,
-      ERROR
-    );
-    // console.error("SendGrid Error:", error.response?.body || error.message);
-  }
+  };
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      throw appError.createError(
+        "Error Occur When Send Reset Password Email Please try again",
+        400,
+        ERROR
+      );
+    }
+  });
 };
 
 module.exports = sendResetPasswordEmail;
+// require("dotenv").config();
+// const sgMail = require("@sendgrid/mail");
+// const appError = require("./appError");
+// const { ERROR } = require("./httpStatus");
+
+
+// const sendResetPasswordEmail = async (email, token) => {
+//   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+//   try {
+//     const url = `${process.env.FRONTEND_URL}/change_password/${token}`;
+//     const message = {
+//       from: process.env.EMAIL,
+//       to: email,
+//       subject: "Reset Password Request",
+//       html: `
+//         <h1>Reset Password Request</h1>
+//         <p>You requested a password reset for your Med VQA account.</p>
+//         <p>Please click the following link to reset your password:</p>
+//         <a href=${url}>Reset Password</a>
+//         <p>If you did not request a password reset, please ignore this email.</p>
+//       `,
+//     };
+
+//     await sgMail.send(message);
+//   } catch (error) {
+//     throw appError.createError(
+//       "Error Occurred While Sending Reset Password Email. Please Try Again.",
+//       400,
+//       ERROR
+//     );
+//     // console.error("SendGrid Error:", error.response?.body || error.message);
+//   }
+// };
+
+// module.exports = sendResetPasswordEmail;

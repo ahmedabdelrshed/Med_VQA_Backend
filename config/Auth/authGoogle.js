54d6422eb077a2c989b53cda4fe964204e4841c2
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-05-10 15:01:23 +0300

    update auth with google to get gender and DOB

--- config/Auth/authGoogle.js
+++ config/Auth/authGoogle.js
@@ -3,6 +3,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const jwt = require("jsonwebtoken");
 const User = require("../../models/userModel");
 const createToken = require("../../utils/createToken");
+const axios = require("axios");
 require("dotenv").config();
 
 passport.use(
@@ -11,16 +12,41 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "https://med-vqa-backend.vercel.app/auth/google/callback",
+      scope: [
+        "profile",
+        "email",
+        "https://www.googleapis.com/auth/user.gender.read",
+        "https://www.googleapis.com/auth/user.birthday.read",
+      ],
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
+        const peopleData = await axios.get(
+          "https://people.googleapis.com/v1/people/me?personFields=genders,birthdays",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        const gender = peopleData.data.genders?.[0]?.value;
+        const birthday = peopleData.data.birthdays?.[0]?.date;
+        const date = new Date(
+          Date.UTC(birthday.year, birthday.month - 1, birthday.day)
+        );
+
         let user = await User.findOne({ email: profile.emails[0].value });
+
         if (!user) {
           user = new User({
             firstName: profile.name.givenName,
             lastName: profile.name.familyName,
             email: profile.emails[0].value,
             isVerified: true,
+            gender: gender.charAt(0).toUpperCase() + gender.slice(1),
+            DateOfBirth: date.toISOString(),
+            password: "",
+            avatar: profile.photos[0].value,
           });
           await user.save();
         }
@@ -32,7 +58,8 @@ passport.use(
           lastName: user.lastName,
           email: user.email,
           avatar: user.avatar,
-        }
+          
+        };
 
         return done(null, { user, token });
       } catch (error) {
